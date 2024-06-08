cbbd9b4f1784b07c79f12a580d1f66392d629aab
ANU7MADHAV <anumadhavan888@gmail>
2024-06-08 08:18:35 +0530

    Feat: Added unimplemented errors in problem controller

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,8 +1,28 @@
 export const pingProblemController = (req, res) => {
   res.json({ message: "pingcontroller server alive" });
 };
-export const addProblem = (req, res) => {};
-export const getProblem = (req, res) => {};
-export const getProblems = (req, res) => {};
-export const deleteProblem = (req, res) => {};
-export const updateProblem = (req, res) => {};
+export const addProblem = (req, res) => {
+  return res.status(501).json({
+    message: "Not implemented",
+  });
+};
+export const getProblem = (req, res) => {
+  return res.status(501).json({
+    message: "Not implemented",
+  });
+};
+export const getProblems = (req, res) => {
+  return res.status(501).json({
+    message: "Not implemented",
+  });
+};
+export const deleteProblem = (req, res) => {
+  return res.status(501).json({
+    message: "Not implemented",
+  });
+};
+export const updateProblem = (req, res) => {
+  return res.status(501).json({
+    message: "Not implemented",
+  });
+};
