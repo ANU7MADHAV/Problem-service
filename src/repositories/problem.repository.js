9adebd319b6607f9f0f0d20c00e2f5e3b445cdc7
ANU7MADHAV <anumadhavan888@gmail>
2024-06-20 15:05:44 +0530

    Feat: Added delete and update problem repository

---
+++ src/repositories/problem.repository.js
@@ -0,0 +1,71 @@
+import Problem from "../models/problem.model.js";
+
+class ProblemRepository {
+  async createProblem(problemData) {
+    try {
+      const problem = await Problem.create({
+        title: problemData.title,
+        description: problemData.description,
+        testCases: problemData.testCases ? problemData.testCases : [],
+      });
+      if (!problem) {
+        return new Error();
+      }
+      return problem;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async getProblems() {
+    try {
+      const problems = await Problem.find();
+      if (!problems) {
+        throw new NotFound("Problem", id);
+      }
+      return problems;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getProblem(id) {
+    try {
+      const problem = await Problem.findById(id);
+      if (!problem) {
+        throw new NotFound("Problem", id);
+      }
+      return problem;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async updateProblem(id, problemData) {
+    try {
+      const updateProblem = await Problem.findByIdAndUpdate(id, problemData, {
+        new: true,
+      });
+      if (!updatedProblem) {
+        throw new NotFound("Problem", id);
+      }
+      return updateProblem;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deleteProblem(id) {
+    try {
+      const problem = await Problem.deleteOne(id);
+      if (!problem) {
+        throw new NotFound("Problem", id);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export default ProblemRepository;
